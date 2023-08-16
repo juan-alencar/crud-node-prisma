caa88f4d21281bdcc2545c648c8f1c78e553d5e5
Juan Alencar <juan.mr.alencar2002@gmail.com>
2023-08-16 00:05:41 -0300

    🐛 bugfix: build test

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,7 @@ app.get<{}, any>("/", (req, res) => {
 
 app.use(urlencoded({ extended: true }));
 app.use("", routerApp);
+
+app.listen(3333, () => console.log(`Server is running in port 3333`));
+
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { PrismaClient } from ".prisma/client";
 import app from "./app";
 
-const port = 3333;
 export const prisma = new PrismaClient();
-
-app.listen(port, () => console.log(`Server is running in port ${port}`));
